d797b4efcd51fa00b176a0ab8e36bba84cb50275
MariMeng <marianabarros.fb@gmail.com>
2025-03-31 15:50:21 -0400

    organizacao inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+//para colocar as rotas
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Ranking from "./pages/Ranking";
+import WhatAlgorithm from "./pages/WhatAlgorithm";
+import WhatError from "./pages/WhatError";
+import WhatLanguage from "./pages/WhatLanguage";
+
+function App(){
+    
+    return(
+        <Router>
+        </Router>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/WhatLanguage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function WhatLanguage(){
+    return (
+    <div> </div>
+    );
+}
+
+export default WhatLanguage;
