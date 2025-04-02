00b5b571580162da5b386ef404c3f020c0fe92b4
MariMeng <marianabarros.fb@gmail.com>
2025-04-01 22:28:58 -0400

    Melhorando a estrutura html da página de quiz do algoritmo e adicionando o css

--- dle-comput/src/pages/WhatAlgorithm.js
+++ dle-comput/src/pages/WhatAlgorithm.js
@@ -1,16 +1,45 @@
 import React from "react";
-
+import "../style.css";
 
 function WhatAlgorithm(){
     return (
         <div> 
         <div>
+            <nav>
+                <p>ranking</p>
+                <p>perfil</p>
+            </nav>
             <div>
-                <p>Acerte o Algoritmo do dia</p>
+                <div class="guia_quiz_horizontal">
+                    <ul>
+                        <li><a href=""><img src="logo192.png" alt="algorithm"></img></a></li>
+                        <li><a href=""><img src="logo192.png" alt="language"></img></a></li>
+                        <li><a href=""><img src="logo192.png" alt="code"></img></a></li>
+                    </ul>
+                </div>
+                <div class="question">
+                    <p>Acerte o Algoritmo do dia</p>
+                </div>
+                <div class="input">
                 <input type="text"></input>
+
+                </div>
+
             </div>
             <div id="certo">
-                <div id="quadrado"></div>
+                <div class="table_answer">
+                    <div class="div_resposta">
+                        <table>
+                            <tr>
+                            
+                            </tr>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="div_git">
+                <p>Confira nosso código no git</p>
             </div>
         </div>
     </div>
